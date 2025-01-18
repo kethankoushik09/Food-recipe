95bcdaa155bec2674e9ee13e4ce77674f181dab2
kethan <kethankoushik09@gmail.com>
2025-01-18 18:32:02 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,39 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import Nav from "./components/nav";
+// import Home from "./pages/home";
+// import Favourites from "./pages/favorites";
+// import Details from "./pages/details";
+// import Customflagcontext from "./context/favcontext";
+
+// function App() {
+//   const rt = createBrowserRouter([
+//     {
+//       path: "/",
+//       element: <Nav />,
+//       children: [
+//         { index:true, element: <Home />},
+//         { path :"fav", element:<Favourites/> },
+//         { path:"/Product-details/:id",element:<Details/>}
+//       ],
+//     },
+//   ]);
+
+//   return (
+//     <Customflagcontext>
+//       <RouterProvider router={rt} 
+//       future={{
+//         v7_startTransition: true, // Include the previous flag if needed
+//         v7_fetcherPersist: true, // Add this to enable the new fetcher behavior
+//       }}
+
+//       />
+//     </Customflagcontext>
+//   );
+// }
+
+// export default App;
+
+import { HashRouter as Router, Route, Routes } from "react-router-dom"; // Import HashRouter
 import Nav from "./components/nav";
 import Home from "./pages/home";
 import Favourites from "./pages/favorites";
@@ -6,27 +41,16 @@ import Details from "./pages/details";
 import Customflagcontext from "./context/favcontext";
 
 function App() {
-  const rt = createBrowserRouter([
-    {
-      path: "/",
-      element: <Nav />,
-      children: [
-        { index:true, element: <Home />},
-        { path :"fav", element:<Favourites/> },
-        { path:"/Product-details/:id",element:<Details/>}
-      ],
-    },
-  ]);
-
   return (
     <Customflagcontext>
-      <RouterProvider router={rt} 
-      future={{
-        v7_startTransition: true, // Include the previous flag if needed
-        v7_fetcherPersist: true, // Add this to enable the new fetcher behavior
-      }}
-
-      />
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/fav" element={<Favourites />} />
+          <Route path="/Product-details/:id" element={<Details />} />
+        </Routes>
+      </Router>
     </Customflagcontext>
   );
 }
