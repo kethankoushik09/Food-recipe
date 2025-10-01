b601ea4bcc190e1c51dc30ab21c046a424c13a24
kethangaikwad <kethan@upgrowthmarketing.in>
2025-10-01 15:41:49 +0530

    changed router mood

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Nav from "./components/nav";
 import Home from "./pages/home";
 import Favourites from "./pages/favorites";
@@ -6,28 +6,17 @@ import Details from "./pages/details";
 import Customflagcontext from "./context/favcontext";
 
 function App() {
-  const rt = createBrowserRouter([
-    {
-      path: "/",
-      element: <Nav />,
-      children: [
-        { index:true, element: <Home />},
-        { path :"fav", element:<Favourites/> },
-        { path:"/Product-details/:id",element:<Details/>}
-      ],
-    },
-  ]);
-
   return (
     <Customflagcontext>
-      <RouterProvider router={rt} 
-      // future={{ v7_startTransition: true }}
-      future={{
-        v7_startTransition: true, // Include the previous flag if needed
-        v7_fetcherPersist: true, // Add this to enable the new fetcher behavior
-      }}
-
-      />
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Nav />}>
+            <Route index element={<Home />} />
+            <Route path="fav" element={<Favourites />} />
+            <Route path="Product-details/:id" element={<Details />} />
+          </Route>
+        </Routes>
+      </HashRouter>
     </Customflagcontext>
   );
 }
